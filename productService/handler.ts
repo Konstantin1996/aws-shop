211062e991c01523e1b14421c8ec1984fc99eda3
Konstantin Zharich <kostyasupikos@gmail.com>
2020-11-03 16:41:19 +0300

    [AWS-TASK-3] - Lambda functions be part
    - added fake request to get cat picture and return it as lamda payload

--- productService/handler.ts
+++ productService/handler.ts
@@ -3,7 +3,17 @@ import 'source-map-support/register';
 import products from './mocks/products.json';
 
 export const getProductsList: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent, context: Context) => {
+  /* GET CAT PICTURE WITH AWAIT
 
+    import axios from 'axios'
+    const axios = require('axios');
+    const { data: { file } } = await axios.get('https://aws.random.cat/meow?ref=apilist.fun');
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ file })
+    }
+  */
   return {
     statusCode: 200,
     headers: {
